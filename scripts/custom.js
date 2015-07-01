c6713f2065f9ae87ee51a903adfdb1d7921491f1
Daniel Park <daniel@summerofdev.co.nz>
2015-07-01 15:51:41 +1200

    Added custom js file

---
+++ scripts/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	// CUSTOM
+	var $logo = $("a.header-brand");
+	$(window).scroll(function () {
+	    var e = $(this).scrollTop();
+	    if (e > 90) {
+	       $logo.addClass('scroll-shrink');
+	    } else {
+	       $logo.removeClass('scroll-shrink');
+	    }
+	});
+});
